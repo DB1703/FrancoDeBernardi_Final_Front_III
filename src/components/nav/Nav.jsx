3699f7173eac35aa979b04a0a7d1be54b6479a26
DB1703 <debernardifranco2003@gmail.com>
2023-07-01 01:58:14 -0300

    Theme Context

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,29 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../../contexts/ThemeContext";
 import '../nav/nav.css'
 
 function Nav() {
-    const [theme, setTheme] = useState(false)
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    const [moon, setMoon] = useState(false);
+
+    const handleButtonClick = () => {
+        toggleTheme();
+        setMoon(!moon)
+    };
+
+
     return (
         <>
             <header>
-                <nav>
+                <nav className={`nav ${theme === 'light' ? 'nav-light' : 'nav-dark'}`}>
                         <h1>Franco's Clinic</h1>
                                 <Link to={"/"}>Home</Link>
                                 <Link to={"dentistas"}>Dentistas</Link>
                                 <Link to={"destacados"}>Destacados</Link>
                                 <Link to={"contacto"}>Contacto</Link>
-                            <button>
-                            {!theme ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+                            <button onClick={handleButtonClick}>
+                            {!moon ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
                                         <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
                                 30</svg>
                                     : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
